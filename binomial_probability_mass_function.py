c21bbdadad29fb863bcfba57fdc9eb2f1e59c22a
Alper Karaca <thisiseternity12@gmail.com>
2026-01-30 09:07:48 +0300

    Binomial Probability Mass Function

---
+++ binomial_probability_mass_function.py
@@ -0,0 +1,10 @@
+import numpy as np
+from scipy.special import comb
+
+def binomial_pmf_cdf(n, p, k):
+    """
+    Compute Binomial PMF and CDF.
+    """
+    pmf = (comb(n, k, exact=True) * (p ** k) * ((1 - p) ** (n - k)))
+    cdf = sum((comb(n, i, exact=True) * (p ** i) * ((1 - p) ** (n - i))) for i in range(k + 1))
+    return float(pmf), float(cdf)
