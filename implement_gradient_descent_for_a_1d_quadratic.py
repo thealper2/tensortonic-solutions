f571c1fe3c061a5391e33065be6d124b5c6d62fe
Alper Karaca <thisiseternity12@gmail.com>
2026-01-31 18:56:32 +0300

    Implement Gradient Descent for a 1D Quadratic

---
+++ implement_gradient_descent_for_a_1d_quadratic.py
@@ -0,0 +1,10 @@
+def gradient_descent_quadratic(a, b, c, x0, lr, steps):
+    """
+    Return final x after 'steps' iterations.
+    """
+    x = x0
+    for _ in range(steps):
+        grad = 2 * a * x + b
+        x -= lr * grad
+
+    return float(x)
