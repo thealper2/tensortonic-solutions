825b19a6a8c6d7b3bf17bd6f8ec7d11d43e99528
Alper Karaca <thisiseternity12@gmail.com>
2026-01-31 18:56:07 +0300

    Implement Huber Loss

---
+++ implement_huber_loss.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def huber_loss(y_true, y_pred, delta=1.0):
+    """
+    Compute Huber Loss for regression.
+    """
+    y_true = np.array(y_true)
+    y_pred = np.array(y_pred)
+    error = y_true - y_pred
+    abs_error = np.abs(error)
+
+    loss = np.where(
+        abs_error <= delta,
+        0.5 * (error ** 2),
+        delta * (abs_error - 0.5 * delta)
+    )
+    
+    return np.mean(loss)
