0e3ac431a49dc8c174364afd23740a3bc4eeb1bb
Alper Karaca <thisiseternity12@gmail.com>
2026-02-01 14:32:14 +0300

    Intersection over Union (IoU)

---
+++ easy/intersection_over_union_iou.py
@@ -0,0 +1,21 @@
+def iou(box_a, box_b):
+    """
+    Compute Intersection over Union of two bounding boxes.
+    """
+    x1 = max(box_a[0], box_b[0])
+    y1 = max(box_a[1], box_b[1])
+    x2 = min(box_a[2], box_b[2])
+    y2 = min(box_a[3], box_b[3])
+
+    inter_w = max(0.0, x2 - x1)
+    inter_h = max(0.0, y2 - y1)
+    intersection = inter_w * inter_h
+
+    area_a = max(0.0, box_a[2] - box_a[0]) * max(0.0, box_a[3] - box_a[1])
+    area_b = max(0.0, box_b[2] - box_b[0]) * max(0.0, box_b[3] - box_b[1])
+
+    union = area_a + area_b - intersection
+    if union == 0.0:
+        return 0.0
+
+    return intersection / union
