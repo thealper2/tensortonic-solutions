1b8f10ef8710699a56cc98634603421dac1dfd51
Alper Karaca <thisiseternity12@gmail.com>
2026-02-01 14:43:44 +0300

    Bernoulli Probability Mass Function & Moments

---
+++ easy/bernoulli_probability_mass_function_and_moments.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def bernoulli_pmf_and_moments(x, p):
+    """
+    Compute Bernoulli PMF and distribution moments.
+    """
+    x = np.asarray(x)
+    pmf = np.where(x == 1, p, 1 - p).astype(float)
+    mean = float(p)
+    var = float(p * (1 - p))
+    return pmf, mean, var
