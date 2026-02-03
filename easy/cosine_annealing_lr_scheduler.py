d002c7d605d2b55ad18bfd2dc33c1cf582858dfd
Alper Karaca <thisiseternity12@gmail.com>
2026-02-03 21:25:15 +0300

    Cosine Annealing LR Scheduler

---
+++ easy/cosine_annealing_lr_scheduler.py
@@ -0,0 +1,8 @@
+import math
+
+def cosine_annealing_schedule(base_lr, min_lr, total_steps, current_step):
+    """
+    Compute the learning rate using cosine annealing.
+    """
+    lr = min_lr + 0.5 * (base_lr - min_lr) * (1 + math.cos(math.pi * current_step / total_steps))
+    return lr
